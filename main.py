75b4f4c6081f15c246b0117fdf5535c979b2e61e
dentrunov <sgema@rambler.ru>
2022-11-29 22:05:25 +0300

    second_commit

---
+++ main.py
@@ -0,0 +1,64 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+import asyncio, aioschedule, sqlite3
+from datetime import datetime, timedelta
+
+from config import TOKEN
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+#список пользователей, заменить на БД
+users = []
+with open('users.txt') as file:
+    users = list(map(int, file.readlines()))
+
+async def mess():
+    conn = sqlite3.connect('FACES.db')
+    cur = conn.cursor()
+    # за какое время нужен отчет о пришедших
+    DELTA_TIME = 6
+    # определяем разницу во времени с текущего времени
+    delta = datetime.now() - timedelta(hours=DELTA_TIME)
+    # делаем выборку
+    cur.execute('''SELECT name, date FROM faces
+                    JOIN attend3 
+                    WHERE faces.userid = attend3.userid AND
+                     date>? AND date <?;''', (delta, datetime.now()))
+    m = list(map(lambda x: x[0] + ': ' + datetime.strptime(x[1], '%Y-%m-%d %H:%M:%S.%f').strftime("%d.%m.%Y, %H:%M:%S"), cur.fetchall()))
+    #m = cur.fetchall()
+    print(m)
+    for user in users:
+        message = '\n'.join(m)
+        await bot.send_message(user, message)
+
+async def scheduler():
+    #aioschedule.every().day.at("17:00").do(mess)
+    aioschedule.every(1).minutes.do(mess)
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(1)
+
+
+async def on_startup(_):
+    asyncio.create_task(scheduler())
+
+
+#Обработка старта
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    global users
+
+    if message.chat.id not in users:
+        #сохранение пользователя
+        print((message.chat.id))
+        users += [message.chat.id]
+        with open('users.txt', 'a') as file:
+            file.write(str(message.chat.id) + '\n')
+
+    await message.reply("Привет!")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, on_startup=on_startup)
--- report_script.py
+++ report_script.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 conn = sqlite3.connect('FACES.db')
 cur = conn.cursor()
 #за какое время нужен отчет о пришедших
-DELTA_TIME = 2
+DELTA_TIME = 6
 #определяем разницу во времени с текущего времени
 delta = datetime.now() - timedelta(hours=DELTA_TIME)
 #делаем выборку
@@ -15,9 +15,13 @@ cur.execute('''SELECT name, date FROM faces
                 JOIN attend3 
                 WHERE faces.userid = attend3.userid AND
                  date>? AND date <?;''', (delta, datetime.now()))
+#print(cur.fetchall())
+m = list(map(lambda x: x[0] + ':' + datetime.strptime(x[1], '%Y-%m-%d %H:%M:%S.%f').strftime("%d.%m.%Y, %H:%M:%S"), cur.fetchall()))
+#m = list(map(lambda x: datetime.strptime(x[1], '%Y-%m-%d %H:%M:%S.%f').strftime("%d.%m.%Y, %H:%M:%S"), cur.fetchall()))
+print(m)
 
-
-print(cur.fetchall())
+'''for x in cur.fetchall():
+    print(x[1])'''
 
 cur.close()
 conn.close()
